96a5a32b203c34d7c68d3e596638a6d2beb842dc
Antonio Gallo <antoniog.web@gmail.com>
2026-01-13 17:29:40 +0100

    git cache cleared

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'antoniogweb/php2xai-mnist',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c975513f91027de34d83ba133d055fe23c719ca9',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'antoniogweb/php2xai' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dbd6e7bc1194fbcc83a49e565312c7fd637bb80d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../antoniogweb/php2xai',
+            'aliases' => array(
+                0 => '0.1.0',
+            ),
+            'dev_requirement' => false,
+        ),
+        'antoniogweb/php2xai-mnist' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c975513f91027de34d83ba133d055fe23c719ca9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
