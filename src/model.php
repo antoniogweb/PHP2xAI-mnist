1823917bd540d86e2920cf99db72b04a80e54466
Antonio Gallo <antoniog.web@gmail.com>
2026-01-20 17:39:31 +0100

    model uses new Tensor logic

--- src/model.php
+++ src/model.php
@@ -1,28 +1,22 @@
 <?php
 
 use PHP2xAI\Models\Model;
-use PHP2xAI\Tensor\Matrix;
-use PHP2xAI\Tensor\Vector;
+// use PHP2xAI\Tensor\Matrix;
+// use PHP2xAI\Tensor\Vector;
 use PHP2xAI\Tensor\Tensor;
 use PHP2xAI\Runtime\PHP\Optimizers\Optimizer;
 
 class MnistModel extends Model
 {
-	protected int $hidden1;
-	protected int $hidden2;
-	
 	public function __construct(?Optimizer $optimizer = null, int $hidden1 = 128, int $hidden2 = 64)
 	{
-		$this->hidden1 = $hidden1;
-		$this->hidden2 = $hidden2;
+		$this->W1 = Tensor::init([784, $hidden1], 0.05);
+		$this->W2 = Tensor::init([$hidden1, $hidden2], 0.05);
+		$this->W3 = Tensor::init([$hidden2, 10], 0.05);
 		
-		$this->W1 = Matrix::init(784, $hidden1, 0.05);
-		$this->W2 = Matrix::init($hidden1, $hidden2, 0.05);
-		$this->W3 = Matrix::init($hidden2, 10, 0.05);
-		
-		$this->b1 = Vector::zeros($hidden1);
-		$this->b2 = Vector::zeros($hidden2);
-		$this->b3 = Vector::zeros(10);
+		$this->b1 = Tensor::zeros([$hidden1]);
+		$this->b2 = Tensor::zeros([$hidden2]);
+		$this->b3 = Tensor::zeros([10]);
 		
 		parent::__construct($optimizer);
 	}
